ffd9c7a06607cae9eccea3938ea6211a1fad1c55
anathayna <anathaynafranca@gmail.com>
2020-04-11 18:45:37 -0300

    adding first exercice

---
+++ 1/1.1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 18:17:32 2020
+
+@author: anathayna
+"""
+print("Hellow")
+print("H","e","l","l","o","w", sep="")
+print("H","e","l","l","o","w", sep="-")
+print("H","e","l","l","o","w", sep=" ")
+print("H","e","l","l","o","w", end="!\n")
+
+pais = "Brasil"
+print(type(pais))
+
+quantidade = 5
+print(type(quantidade))
+
+print(pais, quantidade)
+
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+dia = 15
+mes = 10
+ano = 2015
+print(dia, mes, ano, sep="/")
+
+decimal = 3.1
+print(type(decimal))
